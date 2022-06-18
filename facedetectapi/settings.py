fbe8e85acd7cfe5f828cfb0e4471c1fe35673a59
LENOVO <nrtechnologyid@gmail.com>
2022-06-18 13:32:40 +0700

    JWT

--- facedetectapi/settings.py
+++ facedetectapi/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-rfgc0@$6kbzaz^9uvisp#_6m6bfeie@wo424f4s==t!444^+=#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
